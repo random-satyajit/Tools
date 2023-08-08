b03bb3b99ba0396b166afcc6ae47b7bcb38a2f59
Arbaz ali <30619186+Arbaxali@users.noreply.github.com>
2023-08-08 12:38:54 +0530

    Create jsontest.py
    added how to read and clean the data

---
+++ jsontest.py
@@ -0,0 +1,14 @@
+import json
+ 
+# Opening JSON file
+f = open(r'C:\Work\Jsonformatter\test.json')
+ 
+# returns JSON object as
+# a dictionary
+data = json.load(f)
+dicti = {}
+for key, val in data['Info'].items():
+    dicti[key] = val
+[print(key, ":", val) for key, val in dicti.items()]
+# Closing file
+f.close()
